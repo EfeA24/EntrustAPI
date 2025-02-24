668fbe0c0d9fee3ecf4c693842bf2e141bb0da33
EfeA24 <157731616+EfeA24@users.noreply.github.com>
2025-02-24 16:03:32 +0300

    update

---
+++ WebAPI/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repositories.EfCore;
+
+namespace BookApp.ContextFactory
+{
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Development.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlServer(config.GetConnectionString("DefaultConnection"),
+                prj => prj.MigrationsAssembly("WebAPI"));
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
---
+++ WebAPI/Migrations/20250224130057_initalCreate.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class initalCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    EmployeePosition = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    EmployeeDepartment = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employees", x => x.EmployeeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "items",
+                columns: table => new
+                {
+                    ItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ItemName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ItemQuantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_items", x => x.ItemId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "entrusts",
+                columns: table => new
+                {
+                    EntrustId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    ItemId = table.Column<int>(type: "int", nullable: false),
+                    EntrustDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_entrusts", x => x.EntrustId);
+                    table.ForeignKey(
+                        name: "FK_entrusts_employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "employees",
+                        principalColumn: "EmployeeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_entrusts_items_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "items",
+                        principalColumn: "ItemId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_entrusts_EmployeeId",
+                table: "entrusts",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_entrusts_ItemId",
+                table: "entrusts",
+                column: "ItemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "entrusts");
+
+            migrationBuilder.DropTable(
+                name: "employees");
+
+            migrationBuilder.DropTable(
+                name: "items");
+        }
+    }
+}
